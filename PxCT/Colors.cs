1e3fec452697860c18f159dd93fe0ddb8a4f49b9
Pownylover <ravetrash@gmx.de>
2021-01-29 08:49:20 +0100

    Added color codes
    Storing canvas in array

---
+++ PxCT/Colors.cs
@@ -0,0 +1,45 @@
+﻿namespace PxCT
+{
+    using System.Drawing;
+
+    public static class CanvasColor
+    {
+        #region Properties
+
+        // Source of color names: http://people.csail.mit.edu/jaffer/Color/resenecolours.txt
+
+        public static Color Blue { get; } = Color.FromArgb(0, 0, 234);
+
+        public static Color CapePalliser { get; } = Color.FromArgb(160, 106, 66);
+
+        public static Color CarnationPink { get; } = Color.FromArgb(255, 167, 209);
+
+        public static Color Conifer { get; } = Color.FromArgb(148, 224, 68);
+
+        public static Color FreshEggplant { get; } = Color.FromArgb(130, 0, 128);
+
+        public static Color Gray { get; } = Color.FromArgb(136, 136, 136);
+
+        public static Color Green { get; } = Color.FromArgb(2, 190, 1);
+
+        public static Color Lavender { get; } = Color.FromArgb(207, 110, 228);
+
+        public static Color Lochmara { get; } = Color.FromArgb(0, 131, 199);
+
+        public static Color Mercury { get; } = Color.FromArgb(228, 228, 228);
+
+        public static Color MineShaft { get; } = Color.FromArgb(34, 34, 34);
+
+        public static Color Red { get; } = Color.FromArgb(229, 0, 0);
+
+        public static Color RobinsEggBlue { get; } = Color.FromArgb(0, 211, 221);
+
+        public static Color Tangerine { get; } = Color.FromArgb(229, 149, 0);
+
+        public static Color Turbo { get; } = Color.FromArgb(229, 217, 0);
+
+        public static Color White { get; } = Color.FromArgb(255, 255, 255);
+
+        #endregion
+    }
+}
--- PxCT/MainWindow.xaml.cs
+++ PxCT/MainWindow.xaml.cs
@@ -5,21 +5,88 @@
     using System.Drawing;
     using System.IO;
     using System.Net.Http;
+    using System.Threading.Tasks;
     using System.Windows;
+    using System.Windows.Input;
     using Point = System.Drawing.Point;
 
     /// <summary>Interaction logic for MainWindow.xaml</summary>
     public partial class MainWindow : Window
     {
+        #region Fields
+
+        private int[,] canvas;
+
+        #endregion
+
         public MainWindow()
         {
             this.InitializeComponent();
 
+            this.RefreshCommand = new DelegateCommand(o => this.ExecuteRefresh());
+            this.DataContext = this;
+        }
+
+        #region Properties
+
+        public ICommand RefreshCommand { get; }
+
+        #endregion
+
+        #region Methods
+
+        private static Color GetColor(int colorCode)
+        {
+            return colorCode switch
+            {
+                0 => CanvasColor.White,
+                1 => CanvasColor.Mercury,
+                2 => CanvasColor.Gray,
+                3 => CanvasColor.MineShaft,
+                4 => CanvasColor.CarnationPink,
+                5 => CanvasColor.Red,
+                6 => CanvasColor.Tangerine,
+                7 => CanvasColor.CapePalliser,
+                8 => CanvasColor.Turbo,
+                9 => CanvasColor.Conifer,
+                10 => CanvasColor.Green,
+                11 => CanvasColor.RobinsEggBlue,
+                12 => CanvasColor.Lochmara,
+                13 => CanvasColor.Blue,
+                14 => CanvasColor.Lavender,
+                15 => CanvasColor.FreshEggplant,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <remarks>Chunk IDs from top-left to bottom-right</remarks>
+        /// <param name="chunkId"></param>
+        private void AddChunk(int subChunkId, ICollection<int> colors)
+        {
+        }
+
+        private async void ExecuteRefresh()
+        {
             // TODO: Determine needed chunk area
             var chunkTL = new Point(0, 0);
             var chunkBR = new Point(0, 0);
 
-            this.GetCanvas();
+            this.canvas = await this.GetCanvasAsync();
+
+            // DEBUG
+            var bmp = new Bitmap(960, 960);
+            for (var x = 0; x < this.canvas.GetUpperBound(0); x++)
+            {
+                for (var y = 0; y < this.canvas.GetUpperBound(1); y++)
+                {
+                    var color = GetColor(this.canvas[x, y]);
+                    bmp.SetPixel(x, y, color);
+                }
+            }
+
+            bmp.Save(@"D:\test.bmp");
 
             // 2. check one pixel
 
@@ -34,43 +101,7 @@
             // 7. add refresh button
         }
 
-        #region Methods
-
-        private static void GetColor(Bitmap bmp, int x, int y, int colorcode)
-        {
-            var color = colorcode switch
-            {
-                0 => Color.White,
-                1 => Color.LightGray,
-                2 => Color.Gray,
-                3 => Color.Black,
-                4 => Color.Pink,
-                5 => Color.Red,
-                6 => Color.Orange,
-                7 => Color.Brown,
-                8 => Color.Yellow,
-                9 => Color.GreenYellow,
-                10 => Color.Green,
-                11 => Color.DeepSkyBlue,
-                12 => Color.SteelBlue,
-                13 => Color.DarkBlue,
-                14 => Color.MediumPurple,
-                15 => Color.Purple,
-                _ => throw new NotImplementedException()
-            };
-
-            bmp.SetPixel(x, y, color);
-        }
-
-        /// <summary>
-        /// </summary>
-        /// <remarks>Chunk IDs from top-left to bottom-right</remarks>
-        /// <param name="chunkId"></param>
-        private void AddChunk(int subChunkId, ICollection<int> colors)
-        {
-        }
-
-        private async void GetCanvas()
+        private async Task<int[,]> GetCanvasAsync()
         {
             using var client = new HttpClient();
             var response = await client.GetStreamAsync("https://api.pixelcanvas.io/api/bigchunk/0.0.bmp");
@@ -96,7 +127,7 @@
             var width = 960;
             var height = 960;
 
-            var bmp = new Bitmap(width, height);
+            var bmp = new int[width, height];
 
             for (var horChunkId = 0; horChunkId < 15; horChunkId++)
             {
@@ -106,14 +137,24 @@
                     {
                         for (var y = 0; y < 64; y++)
                         {
-                            var pixelIndex = ((horChunkId + (verChunkId * 15)) * 4096) + x + (y * 64);
-                            GetColor(bmp, (horChunkId * 64) + x, (verChunkId * 64) + y, colorCodes[pixelIndex]);
+                            try
+                            {
+                                var pixelIndex = ((horChunkId + (verChunkId * 15)) * 4096) + x + (y * 64);
+                                bmp[(horChunkId * 64) + x, (verChunkId * 64) + y] = colorCodes[pixelIndex];
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e);
+                                throw;
+                            }
+
+                            //GetColor(bmp, (horChunkId * 64) + x, (verChunkId * 64) + y, colorCodes[pixelIndex]);
                         }
                     }
                 }
             }
 
-            bmp.Save(@"D:\test.bmp");
+            return bmp;
         }
 
         #endregion
